382941386d4964b0631b51898b9eb9079e040ecb
manojubale <manojubale93@gmail.com>
2026-04-05 10:35:25 +0000

    local

--- test/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "demo-manoj-static-website-bucket"   # must be globally unique
-    key    = "ec2-test-app/terraform.tfstate"       # path inside bucket
-    region = "us-east-2"                   # Ohio region
-  }
-}
