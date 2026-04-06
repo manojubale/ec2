9c4acb88f364040b03ad7c843804bf19c8b3285a
manojubale <manojubale93@gmail.com>
2026-04-06 04:25:49 +0000

    prod-deployment

--- dev/userdata.sh
+++ dev/userdata.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
-
-sudo apt update -y
-sudo apt install nginx -y
-
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
-# Create a custom HTML page
-cat <<EOF > /var/www/html/index.html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Manoj DevOps Project</title>
-    <style>
-        body {
-            background-color: #0f172a;
-            color: white;
-            text-align: center;
-            font-family: Arial;
-            padding-top: 100px;
-        }
-        h1 {
-            font-size: 50px;
-            color: #38bdf8;
-        }
-    </style>
-</head>
-<body>
-    <h1>🚀 Welcome Manoj!</h1>
-    <p>Jenkins + Terraform + AWS EC2 + NGINX Working ✅</p>
-</body>
-</html>
-EOF
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo echo "<h1>fullsatack-dev-app</h1>" > /var/www/html/index.html
--- prod/userdata.sh
+++ prod/userdata.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
-
-# Update system
-apt update -y
-
-# Install NGINX
-apt install nginx -y
-
-# Start and enable NGINX
-systemctl start nginx
-systemctl enable nginx
-
-# Create a custom HTML page
-cat <<EOF > /var/www/html/index.html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Manoj DevOps Project</title>
-    <style>
-        body {
-            background-color: #0f172a;
-            color: white;
-            text-align: center;
-            font-family: Arial;
-            padding-top: 100px;
-        }
-        h1 {
-            font-size: 50px;
-            color: #38bdf8;
-        }
-    </style>
-</head>
-<body>
-    <h1>🚀 Welcome Manoj!</h1>
-    <p>Jenkins + Terraform + AWS EC2 + NGINX Working ✅</p>
-</body>
-</html>
-EOF
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo echo "<h1>fullsatack-prod-app</h1>" > /var/www/html/index.html
